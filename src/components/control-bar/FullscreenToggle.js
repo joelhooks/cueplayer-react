ef9add3517830c7ee32edd57300cbe05610616db
Joel Hooks <joel@egghead.io>
2021-04-24 13:18:12 -0700

    Update FullscreenToggle.js

--- src/components/control-bar/FullscreenToggle.js
+++ src/components/control-bar/FullscreenToggle.js
@@ -1,39 +1,40 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import classNames from 'classnames';
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import classNames from 'classnames'
 
 const propTypes = {
   actions: PropTypes.object,
   player: PropTypes.object,
-  className: PropTypes.string
-};
+  className: PropTypes.string,
+  fullscreenElement: PropTypes.any,
+}
 
 export default class FullscreenToggle extends Component {
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props)
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-    const { player, actions } = this.props;
-    actions.toggleFullscreen(player);
+    const {player, actions, fullscreenElement} = this.props
+    actions.toggleFullscreen(player, fullscreenElement)
   }
 
   render() {
-    const { player, className } = this.props;
+    const {player, className} = this.props
     return (
       <button
         className={classNames(
           className,
           {
             'cueplayer-react-icon-fullscreen-exit': player.isFullscreen,
-            'cueplayer-react-icon-fullscreen': !player.isFullscreen
+            'cueplayer-react-icon-fullscreen': !player.isFullscreen,
           },
-          'cueplayer-react-fullscreen-control cueplayer-react-control cueplayer-react-button cueplayer-react-icon'
+          'cueplayer-react-fullscreen-control cueplayer-react-control cueplayer-react-button cueplayer-react-icon',
         )}
         ref={c => {
-          this.button = c;
+          this.button = c
         }}
         type="button"
         tabIndex="0"
@@ -41,9 +42,9 @@ export default class FullscreenToggle extends Component {
       >
         <span className="cueplayer-react-control-text">Non-Fullscreen</span>
       </button>
-    );
+    )
   }
 }
 
-FullscreenToggle.propTypes = propTypes;
-FullscreenToggle.displayName = 'FullscreenToggle';
+FullscreenToggle.propTypes = propTypes
+FullscreenToggle.displayName = 'FullscreenToggle'
